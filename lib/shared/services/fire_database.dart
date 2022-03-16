3a81eae89e19d33edab33c7ea7f5dd0ed0d0c7bc
Daniel Luiz Lopes Magri - DMA <daniel.magri@C02F41WNMD6P.local>
2022-03-16 15:27:48 -0300

    Libs version updated

--- lib/shared/services/fire_database.dart
+++ lib/shared/services/fire_database.dart
@@ -1,5 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart'
-    show CollectionReference, FirebaseFirestore;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:injectable/injectable.dart';
 
